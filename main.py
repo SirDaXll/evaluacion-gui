6d53403391215f3429dfc7d0f935f932362d32aa
El Diego <SirDaXll@users.noreply.github.com>
2023-05-08 12:01:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,24 @@
 # 1 Lt = 2,11 Pintas
 
 class Medida():
-    def __init__(nombreMedida, valorMedida, valorEquivalente, ):
-        valorMedida = 0
-        equivalenteA1Lt = 0
-        valorEquivalente = valorMedida/equivalenteA1Lt
+    def __init__(self, nombreMedida, valorMedida):
+        self.nombreMedida = nombreMedida.lower()
+        self.valorMedida = int(valorMedida)
+        if self.nombreMedida == 'galones':
+            self.equivalenteA1Lt = int(0,26)
+        elif self.nombreMedida == 'pintas':
+            self.equivalenteA1Lt = int(2,11)
+        self.valorEquivalente = self.valorMedida/self.equivalenteA1Lt
+
+    def __str__(self):
+        return f'{self.nombreMedida}: {self.valorMedida} ({self.valorEquivalente} Lt)'
+    
+    def comparar(self):
+        x1 = 0
+        x2 = 0
+        if x1 < x2:
+            print(f'{x1} < {x2}')
+        elif x1 > x2:
+            print(f'{x1} > {x2}')
+        elif x1 == x2:
+            print(f'{x1} = {x2}')
