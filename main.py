4a03bd8184751f06cbdfd805b8e8ad90a5fb47c5
El Diego <SirDaXll@users.noreply.github.com>
2023-05-08 11:14:08 -0400

    si

---
+++ main.py
@@ -0,0 +1,12 @@
+# Grupo 11: Galón vs Pinta
+# La clase es Medida y requiere de los atributos de nombre medida, valor medida (en el valor de la medida) y valor equivalente (en el valor en Lt).
+# La formula es: valorEquivalente = valorMedida/equivalenteA1Lt
+# Información equivalente a 1 KM:
+# 1 Lt = 0,26 Galones
+# 1 Lt = 2,11 Pintas
+
+class Medida():
+    def __init__(nombreMedida, valorMedida, valorEquivalente, ):
+        valorMedida = 0
+        equivalenteA1Lt = 0
+        valorEquivalente = valorMedida/equivalenteA1Lt
